607265c581ac7c834aeb86ab70f1f109fa4f8766
lolyparty <ololadeolayiwola01@gmail.com>
2022-03-28 12:57:51 +0100

    updated the get request to curated pictures, increased size of the photos and made them clearer

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ function App() {
   //axios instance
   const instance = axios.create({baseURL:'https://api.pexels.com/v1/'})
 
+  //Get photos
   const getPhotos = async ()=>{
     try{
-      const photos = await instance.get('/search?query=nature?page=1&per_page=10',{
+      const photos = await instance.get('/curated?page=1&per_page=10',{
       headers:{
         Accept: "application/json",
       Authorization:process.env.REACT_APP_INFINITESCROLL
@@ -25,14 +26,13 @@ function App() {
     }
   }
 
+  // react query
   const {data, isLoading, isError, isSuccess} = useQuery('Photos', getPhotos)
   
-
-
   return (
     <div>
       {isLoading && <div>Loading....</div>}
-      {isSuccess && <div className="container">{data.data.photos.map(e=> <div className="img_container" key={e.id}><img className="img_container" alt="" src={e.src.medium}/></div>)}</div>}
+      {isSuccess && <div className="container">{data.data.photos.map(e=> <div className="img_container" key={e.id}><img className="img_container" alt="" src={e.src.large}/></div>)}</div>}
       {isError && <div>Error...</div>}
     </div>
     );
